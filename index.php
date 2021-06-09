9a282457a34bba2e5f03c8f3d63fef97e41fe324
Kandarpa Malipeddi <69784217+kandarpamalipeddi@users.noreply.github.com>
2021-06-09 10:45:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
+    echo "<h1 align='center'> ODO Test page </h1>";
+    echo "Hello User !!! <br> <p> This is a sample php page to be deployed by Odo on OCP clusters. Your request Timestamp : " . date(DATE_RFC2822) . "</p>";
+
     phpinfo();
 ?>
+
